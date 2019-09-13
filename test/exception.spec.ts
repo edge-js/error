016c8044e8dc16b28e867c123f8640e8a16d2639
Harminder virk <virk.officials@gmail.com>
2019-09-13 08:30:19 +0530

    chore(package): update dependencies

--- test/exception.spec.ts
+++ test/exception.spec.ts
@@ -7,7 +7,7 @@
 * file that was distributed with this source code.
 */
 
-import * as test from 'japa'
+import test from 'japa'
 import { EdgeError } from '../src'
 
 test('raise error with given filename', (assert) => {
