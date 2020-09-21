9445d6f19e3d22a675017ec18c30b38cb68c3eb5
thetutlage <virk.officials@gmail.com>
2020-09-21 13:53:53 +0530

    chore: setup prettier and format source files

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,31 @@
 /*
-* edge-error
-*
-* (c) Harminder Virk <virk@adonisjs.com>
-*
-* For the full copyright and license information, please view the LICENSE
-* file that was distributed with this source code.
-*/
+ * edge-error
+ *
+ * (c) Harminder Virk <virk@adonisjs.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 export type ExceptionOptions = {
-  line: number,
-  col: number,
-  filename: string,
+	line: number
+	col: number
+	filename: string
 }
 
 export class EdgeError extends Error {
-  public line: number
-  public col: number
-  public filename: string
+	public line: number
+	public col: number
+	public filename: string
 
-  constructor (public message: string, public code: string, options: ExceptionOptions) {
-    super(message)
-    this.line = options.line
-    this.col = options.col
-    this.filename = options.filename
+	constructor(public message: string, public code: string, options: ExceptionOptions) {
+		super(message)
+		this.line = options.line
+		this.col = options.col
+		this.filename = options.filename
 
-    const stack = this.stack!.split('\n')
-    stack.splice(1, 0, `    at anonymous (${this.filename}:${this.line}:${this.col})`)
-    this.stack = stack.join('\n')
-  }
+		const stack = this.stack!.split('\n')
+		stack.splice(1, 0, `    at anonymous (${this.filename}:${this.line}:${this.col})`)
+		this.stack = stack.join('\n')
+	}
 }
