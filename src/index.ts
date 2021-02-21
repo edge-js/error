dd02a9939a0b1442fc3e5380e454b80bd59f23f0
Harminder Virk <virk.officials@gmail.com>
2021-02-21 22:45:06 +0530

    style: reformat source files

--- src/index.ts
+++ src/index.ts
@@ -8,24 +8,24 @@
  */
 
 export type ExceptionOptions = {
-	line: number
-	col: number
-	filename: string
+  line: number
+  col: number
+  filename: string
 }
 
 export class EdgeError extends Error {
-	public line: number
-	public col: number
-	public filename: string
+  public line: number
+  public col: number
+  public filename: string
 
-	constructor(public message: string, public code: string, options: ExceptionOptions) {
-		super(message)
-		this.line = options.line
-		this.col = options.col
-		this.filename = options.filename
+  constructor(public message: string, public code: string, options: ExceptionOptions) {
+    super(message)
+    this.line = options.line
+    this.col = options.col
+    this.filename = options.filename
 
-		const stack = this.stack!.split('\n')
-		stack.splice(1, 0, `    at anonymous (${this.filename}:${this.line}:${this.col})`)
-		this.stack = stack.join('\n')
-	}
+    const stack = this.stack!.split('\n')
+    stack.splice(1, 0, `    at anonymous (${this.filename}:${this.line}:${this.col})`)
+    this.stack = stack.join('\n')
+  }
 }
--- test/exception.spec.ts
+++ test/exception.spec.ts
@@ -11,22 +11,22 @@ import test from 'japa'
 import { EdgeError } from '../src'
 
 test('raise error with given filename', (assert) => {
-	const error = new EdgeError('unclosed tag slot', 'E_UNCLOSED_TAG', {
-		line: 2,
-		col: 0,
-		filename: 'index.edge',
-	})
-	assert.equal(error.filename, 'index.edge')
-	assert.equal(error.code, 'E_UNCLOSED_TAG')
-	assert.equal(error.line, 2)
-	assert.equal(error.col, 0)
+  const error = new EdgeError('unclosed tag slot', 'E_UNCLOSED_TAG', {
+    line: 2,
+    col: 0,
+    filename: 'index.edge',
+  })
+  assert.equal(error.filename, 'index.edge')
+  assert.equal(error.code, 'E_UNCLOSED_TAG')
+  assert.equal(error.line, 2)
+  assert.equal(error.col, 0)
 })
 
 test('patch error stack with the filename and line:col', (assert) => {
-	const error = new EdgeError('unclosed tag slot', 'E_UNCLOSED_TAG', {
-		line: 2,
-		col: 0,
-		filename: 'index.edge',
-	})
-	assert.equal(error.stack!.split('\n')[1], '    at anonymous (index.edge:2:0)')
+  const error = new EdgeError('unclosed tag slot', 'E_UNCLOSED_TAG', {
+    line: 2,
+    col: 0,
+    filename: 'index.edge',
+  })
+  assert.equal(error.stack!.split('\n')[1], '    at anonymous (index.edge:2:0)')
 })
