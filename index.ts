630b329392cf3ad5fd709cd1486abb57c4c69ada
Julien <julien@ripouteau.com>
2023-07-25 11:20:08 +0200

    chore: use export type

--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@
  * file that was distributed with this source code.
  */
 
-import { ExceptionOptions } from './types.js'
+import type { ExceptionOptions } from './types.js'
 
 export class EdgeError extends Error {
   line: number
